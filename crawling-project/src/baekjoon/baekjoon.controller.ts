0e3ebacac045460c966b71a66cd5586773b4bdcc
haho0304 <haho020304@naver.com>
2024-08-05 17:03:08 +0900

    unique, getBaekjoonOne

--- crawling-project/src/baekjoon/baekjoon.controller.ts
+++ crawling-project/src/baekjoon/baekjoon.controller.ts
@@ -1,17 +1,26 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BaekjoonService } from './baekjoon.service';
 
-@Controller('beakjoon')
+@Controller('baekjoon')
 export class BaekjoonController {
-    constructor(private readonly beakjoonService: BaekjoonService) {}
+  constructor(private readonly baekjoonService: BaekjoonService) {}
 
-    @Post()
-    getBeckjoon(@Body('board') board: string) {
-        return this.beakjoonService.getBeakjoon(board);
-    }
+  @Post('all')
+  getBaekjoon(
+    @Body('id') id: string,
+    @Body('repository') repository: string,
+    @Body('rank') rank: string,
+  ): Promise<string[]> {
+    return this.baekjoonService.getBaekjoon(id, repository, rank);
+  }
 
-    @Post()
-    getBeckjoonOne(@Body('board') board:string) {
-        
-    }
+  @Post('one')
+  getBeckjoonOne(  
+    @Body('id') id: string,
+    @Body('repository') repository: string,
+    @Body('rank') rank: string,
+    @Body('number') number: number,
+  ): Promise<string[]> {
+    return this.baekjoonService.getBaekjoonOne(id, repository, rank, number);
+  }
 }
--- crawling-project/src/baekjoon/baekjoon.service.ts
+++ crawling-project/src/baekjoon/baekjoon.service.ts
@@ -3,7 +3,7 @@ import puppeteer, { executablePath } from 'puppeteer';
 
 @Injectable()
 export class BaekjoonService {
-    async getBeakjoon(board: string) {
+    async getBaekjoon(id: string, repository: string, rank: string) {
         let browser = await puppeteer.launch({
             headless: true,
             executablePath: executablePath('chrome')
@@ -11,15 +11,15 @@ export class BaekjoonService {
 
         try {
             const page = await browser.newPage();
-            await page.goto('https://github.com/PBDott/Algorithm/tree/main/%EB%B0%B1%EC%A4%80/Bronze');
+            await page.goto(`https://github.com/${id}/${repository}/tree/main/%EB%B0%B1%EC%A4%80/${rank}`);
 
             const content = await page.evaluate(() => {
                 // 'Link--primary' 클래스와 'title' 속성을 이용해 요소 선택
                 const elements = Array.from(document.querySelectorAll('a.Link--primary[title]'));
                 return elements.map(element => element.getAttribute('title')?.trim() || '');
             });
-
-            return content;
+            let uniqueContent = Array.from(new Set(content));
+            return uniqueContent;
 
         } catch (error) {
             console.log(error);
@@ -28,4 +28,18 @@ export class BaekjoonService {
             await browser.close();
         }
     } 
+
+    async getBaekjoonOne(id: string, repository: string, rank: string, number: number ) {
+        try {
+            const content = await this.getBaekjoon(id, repository, rank);
+            const numberString = number.toString();
+            const numberContent = content.filter(title => title.slice(0, numberString.length) === numberString);
+
+            return numberContent;
+
+        } catch (error) {
+            console.log(error);
+            throw new Error('크롤링 에러');
+        }
+    }
 }
